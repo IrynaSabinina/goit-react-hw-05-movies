8f4e90a831aa4a2fcf7453011f16434a871cd8f4
Iryna <pysarenkoiryna@gmail.com>
2022-08-09 15:24:13 +0300

    way

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,7 @@ export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/goit-react-hw-05-movies" element={<Layout />}>
           <Route
             index
             element={
